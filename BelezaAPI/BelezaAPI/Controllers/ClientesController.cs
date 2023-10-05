55a5a808b05107c91bd0d51726d2d842d4cf4603
KaykSouza <kayksmr@gmail.com>
2023-10-05 15:27:05 -0300

    Mensagem

---
+++ BelezaAPI/BelezaAPI/Controllers/ClientesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BelezaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+
+        public ClientesController(Contexto ctx)
+        {
+
+
+        }
+        [HttpPost]
+        public ActionResult Post(ClientesController cli)
+        {
+            return Created($"\Clientes/{cli.Id}", cli);
+        }
+    }
+}
---
+++ BelezaAPI/BelezaAPI/Dominio/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace BelezaAPI.Dominio
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string? Apelido { get; set; }
+        public DateTime? DataNascimento { get; set; }
+
+        public bool Bloqueado { get; set; }
+        public double Fiado { get; set; }
+    }
+
+}
