ee20b8dc4f8098c85a8bd95c0d8790d8dcc54ef8
KaykSouza <kayksmr@gmail.com>
2023-10-11 20:25:31 -0300

    Decimal

--- BelezaAPI/BelezaAPI/Dominio/Servico.cs
+++ BelezaAPI/BelezaAPI/Dominio/Servico.cs
@@ -4,7 +4,7 @@ public class Servico
 {
 	public string Nome { get; set; }
 	public int Id { get; set; }
-	public int Preco { get; set; }
+	public decimal Preco { get; set; }
 	public bool Disponibilidade { get; set; }
     public int DuracaoMin { get; set; }
 }
